308345240f619cf78aa5d701649f7b0e3bd9cd42
nellihol <46727219+nellihol@users.noreply.github.com>
2019-09-11 21:05:42 +1000

    Add files via upload

---
+++ Fuel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace totalCostofFuel
+{
+    class Fuel
+    {
+        // The fuel variables
+        public double liters { get; set; }
+        public double cost { get; set; }
+
+        private double totalCost;
+
+        // Calculate the total cost of fuel 
+        public double calculateCost(double _cost, double _liters) {
+            totalCost = _cost * _liters;
+            return totalCost;
+        }
+
+        // Return a message of the calculated values 
+        public new string ToString() {
+            // Print the message with the variable values  and use the calculation method to get the total amount
+            return ("Number of liters of fuel " + liters + " l. Cost of fuel: $"+ cost + ". Total cost: $"+ calculateCost(liters, cost));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace totalCostofFuel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variables for user input values
+            double inputCost;
+            double inputLiters;
+
+            // Starting the console app with a welcome message
+            WriteLine("Welcome to the fuel cost calculator. ");
+            WriteLine("Made by Nelli Holopainen");
+            WriteLine("=============\n");
+
+            // Ask the user over and over again until the value is valid
+            do {
+
+                // Promt the user for the amount of fuel
+                WriteLine("Enter the amount of liters of fuel filled in the car: ");
+                double.TryParse(ReadLine(), out inputLiters);
+
+                if (inputLiters <= 0 )
+                {
+                    WriteLine("Error! Negative values or letters not allowed.");
+                }
+
+            } while (inputLiters <= 0);
+
+            // Ask the user over and over again until the value is valid
+            do
+            {
+                // Promt the user for the cost of fuel
+                WriteLine("Enter the cost in dollars and cents (eg. 20.77): ");
+                double.TryParse(ReadLine(), out inputCost);
+
+                if (inputCost <= 0)
+                {
+                    WriteLine("Error! Negative values or letters not allowed.");
+                }
+
+            } while (inputCost <= 0);
+
+            // New fuel object
+            Fuel fuel = new Fuel();
+
+            // Assign the validated values to object data fields
+            fuel.liters = inputLiters;
+            fuel.cost = inputCost;
+
+            // Print the result using the tostring method
+            WriteLine("=============");
+            WriteLine("The result:");
+            WriteLine(fuel.ToString());
+
+            WriteLine("\nThank you for using fuel cost calculator! Press any key to exit.");
+            ReadLine();
+
+        }
+    }
+}
